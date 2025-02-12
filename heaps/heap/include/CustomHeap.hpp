c4f14fa4240be6f85f0a10ad4818c2f23d8eab24
AleksaArsic <arsicaleksa96@gmail.com>
2025-02-12 10:27:38 +0100

    Heap project skeleton added.

---
+++ heaps/heap/include/CustomHeap.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace CustomHeap
+{
+    class Heap final
+    {
+        public:
+
+        private:
+        
+    };
+}
---
+++ heaps/heap/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "CustomHeap.hpp"
+
+int main()
+{
+    std::cout << "Hello from Heap." << std::endl;
+
+    return 0;
+}
